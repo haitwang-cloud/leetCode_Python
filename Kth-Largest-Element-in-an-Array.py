e8052bf0347fe451676978751dbf439c17bc4ece
seattleboy <wanghaitao@i.shu.edu.cn>
2018-11-05 15:54:11 +0800

    add Kth Largest Element in an Array

---
+++ Kth-Largest-Element-in-an-Array.py
@@ -0,0 +1,18 @@
+# Time:  O(n) ~ O(n^2)
+# Space: O(1)
+
+
+class Solution:
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        #nums.sort()
+        #return nums[len(nums)-k]
+        
+        # import heapq
+        # return heapq.nlargest(k,nums)[-1]
+        
+        return sorted(nums)[-k]
