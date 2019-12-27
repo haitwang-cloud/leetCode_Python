56dddc2158edc40f70300b670add822b72e5bb12
haitwang <haitwang@ebay.com>
2019-12-27 09:10:33 +0800

    add two_sum solution 2

--- Two_Sum_II.py
+++ Two_Sum_II.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    def twoSum_1(self, numbers: List[int], target: int) -> List[int]:
         if len(numbers)==0:
             return [-1,-1]
         left,right=0,len(numbers)-1
@@ -10,3 +10,13 @@ class Solution:
                 left+=1
             elif numbers[left]+numbers[right]>target:
                 right-=1
+    def twoSum_2(self, numbers: List[int], target: int) -> List[int]:
+        # 继续采用hashmap的思想
+        hashmap = {}
+        for index, num1 in enumerate(numbers):
+            num2 = target - num1
+
+            if num2 in hashmap:
+                return [hashmap[num2] + 1, index + 1]
+
+            hashmap[num1] = index
